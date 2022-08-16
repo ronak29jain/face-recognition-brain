0e04af2f5ff8581223197de972db85b90c6a64e7
Ronak Jain <ronak290jain@gmail.com>
2022-08-16 13:49:25 +0530

    created react app and added navigation bar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navigation from './Components/Navigation/Navigation'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>hello world</h1> */}
+      <Navigation />
+      {/* <Logo /> */}
+      {/* <ImageLinkForm /> */}
+      {/* <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navigation.css'
+
+function Navigation() {
+  return (
+    <div className='navigation'>
+      <nav className='nav'>
+        <p className='f3 pa3 link dim black underline pointer'>Sign Out</p>
+      </nav>
+    </div>
+  )
+}
+
+export default Navigation
