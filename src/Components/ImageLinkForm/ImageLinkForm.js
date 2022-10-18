e7da8a4bb47eeab79babe675c98a1f4ff4191846
ronak29jain <ronak290jain@gmail.com>
2022-10-18 22:35:51 +0530

    added placeholder

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -27,7 +27,7 @@ function ImageLinkForm({onInputChange, onSubmit}) {
       <p className='f3 center'>{'This small brain will detect faces in your pictures. Give it a try'}</p>
       <div className="center">
         <div className="pa4 br3 shadow-5 form center">
-          <input id='text-box' onClick={selectText} onKeyPress={handleKeyPress} onChange={onInputChange} className='f4 pa2 w-70 center' type="text" />
+          <input id='text-box' placeholder='Enter Image Url' onClick={selectText} onKeyPress={handleKeyPress} onChange={onInputChange} className='f4 pa2 w-70 center' type="text" />
           <button onClick={detectface} className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
         </div>
       </div>
